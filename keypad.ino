567b8d439575c0be859106cc96a1ff3649aa14cb
theimmc <theimmc@gmail.com>
2016-03-21 11:01:22 -0700

    Update keypad.ino
    Code cleanup, some minor fixes, optimization.

--- keypad.ino
+++ keypad.ino
@@ -1,14 +1,23 @@
-// These are the column pins, to be configured for input with
+#include <keyboard.h>
+
+const int COLUMNS = 4;
+const int ROWS = 5;
+const int HB_LED = 17;
+
+#define HEARTBEAT_ENABLED 1
+
+// These are the column pins. They're configured for input with
 // internal pullup
 
-int input_pins[4] = { 20, 19, 18, 15 } ;
+int input_pins[COLUMNS] = { 20, 19, 18, 15 } ;
 
 // These are the row strobes. They're configured for output
 // and initially all set to high. Individual pins are set to
 // low to read that row. Only one row is low at any time.
 
-int strobe_pins[5] = { 8, 7, 6, 5, 4 };
-unsigned long key_state[5][4];
+int strobe_pins[ROWS] = { 8, 7, 6, 5, 4 };
+
+unsigned long key_state[ROWS][COLUMNS];
 
 // Codes for numeric keypad
 // NUM  /   *   -
@@ -17,11 +26,11 @@ unsigned long key_state[5][4];
 //  1   2   3   x
 //  0   x  . ENTER
 
-int keycode[5][5] = { { 0xDB, 0xDC, 0xDD, 0xDE },
-                      { 0xE7, 0xE8, 0xE9, 0xDF },
-                      { 0xE4, 0xE5, 0xE6, 0    },
-                      { 0xE1, 0xE2, 0xE3, 0 },
-                      { 0xEA, 0,    0xEB, 0XE0 } };
+int keycode[ROWS][COLUMNS] = { { 0xDB, 0xDC, 0xDD, 0xDE },
+                               { 0xE7, 0xE8, 0xE9, 0xDF },
+                               { 0xE4, 0xE5, 0xE6, 0    },
+                               { 0xE1, 0xE2, 0xE3, 0 },
+                               { 0xEA, 0,    0xEB, 0XE0 } };
                       
 int strobe_row = 0;
 int q = 0;
@@ -30,30 +39,32 @@ void setup() {
   int cnt;
   int cnt2;
 
-  // This is for the heartbeat
+#ifdef HEARTBEAT_ENABLED  
+  pinMode(HB_LED, OUTPUT);
+#endif
   
-  pinMode(17, OUTPUT);
-  
-  for (cnt = 0; cnt < 5; cnt++) {
+  for (cnt = 0; cnt < ROWS; cnt++) {
     pinMode(strobe_pins[cnt], OUTPUT);
     digitalWrite(strobe_pins[cnt], HIGH);
 
-    for (cnt2 = 0; cnt2 < 4; cnt2++) key_state[cnt][cnt2] = 0;
+    for (cnt2 = 0; cnt2 < COLUMNS; cnt2++) key_state[cnt][cnt2] = 0;
   }
 
-  for (cnt = 0; cnt < 4; cnt++) {
+  for (cnt = 0; cnt < COLUMNS; cnt++) {
     pinMode(input_pins[cnt], INPUT_PULLUP);
   }
 
   Keyboard.begin();
 }
 
+const int DEBOUNCE_MS = 20;
+
 bool debounce(unsigned long t_now, unsigned long t_prev) {
   unsigned long diff;
 
   diff = t_now - t_prev; // need to check for underflow?
 
-  if (diff <= 20) return true;
+  if (diff <= DEBOUNCE_MS) return true;
   else return false;
 }
 
@@ -61,14 +72,14 @@ void loop() {
   unsigned long tick_now = millis();
   int cnt;
 
-  // Heartbeat LED. Comment this out if you don't want blinkenlights
-  
-  if (q == 0) digitalWrite(17, LOW);
-  else if (q == 128) digitalWrite(17, HIGH);
+#ifdef HEARTBEAT_ENABLED
+  if (q == 0) digitalWrite(HB_LED, LOW);
+  else if (q == 128) digitalWrite(HB_LED, HIGH);
   q++;
-  if (q == 256) q = 0;
-  
-  // end of heartbeat code
+  q &= 0xff;
+  // should just make this into an unsigned char, but keeping it as int
+  // in case we want to adjust heartbeat freq.
+#endif
 
   // since we use non zero to indicate pressed state, we need
   // to handle the edge case where millis() returns 0
@@ -78,21 +89,20 @@ void loop() {
   // every time we enter this loop, we're reading only the switches
   // on strobe_row
   
-  if (strobe_row >= 5) strobe_row = 0;
+  if (strobe_row >= ROWS) strobe_row = 0;
   
   digitalWrite(strobe_pins[strobe_row], LOW);
   delay(2); // give it some time to stabilize just in case
 
   // We check all the switches in the row
 
-  for (cnt = 0; cnt < 4; cnt++) {
+  for (cnt = 0; cnt < COLUMNS; cnt++) {
     // ignore state change for pin if in debounce period
     if (key_state[strobe_row][cnt] != 0)
       if (debounce(tick_now, key_state[strobe_row][cnt]) == true)
         continue;
 
     if (digitalRead(input_pins[cnt]) == HIGH) {
-      Serial.println("released");
       if (key_state[strobe_row][cnt] != 0) {
         Keyboard.release(keycode[strobe_row][cnt]);
         key_state[strobe_row][cnt] = 0;
